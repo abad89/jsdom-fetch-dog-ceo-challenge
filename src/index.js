a6ed55df75f678bc3ff3102a12bc14656e26d9b2
abad89 <abadalamenti89@gmail.com>
2021-04-11 23:38:34 -0400

    added random dog images

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick')
+
+
+const createImages = ((dogArray) => {
+   dogArray.forEach((element) => {
+      const container = document.getElementById(`dog-image-container`)
+      const image = document.createElement(`img`)
+      image.src=element
+      container.appendChild(image)
+   })
+})
+
+
+fetch("https://dog.ceo/api/breeds/image/random/4")
+.then(resp => resp.json())
+.then(json => {
+   dogs = json.message
+   createImages(dogs)
+})
+
+fetch('https://dog.ceo/api/breeds/list/all')
+.then(resp => resp.json())
+.then(json => breeds = json.message)
+.then(info => console.log(breeds))
